913e47c6af097abfa9e37affcdd4e8aabddf7eac
JedlikTZS <tokzskinga@gmail.com>
2020-02-04 14:01:54 +0100

    Feltöltés és kirajzolás.

--- Aknakereso/Program.cs
+++ Aknakereso/Program.cs
@@ -8,17 +8,48 @@ namespace Aknakereso
 {
     class Program
     {
-        char[,] palya = new char[10, 10];
+        
         static void Main(string[] args)
         {
-            Feltoltes();
+            char[,] palya = new char[10, 10];
+            Feltoltes(palya);
+            Kirajzolo(palya);
 
             Console.ReadKey();
         }
 
-        static void Feltoltes()
+        static void Kirajzolo(char[,] palya)
         {
-            
+            for (int i = 0; i < palya.GetLength(0); i++)
+            {
+                for (int j = 0; j < palya.GetLength(1); j++)
+                {
+                    Console.Write(palya[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Feltoltes(char[,] palya)
+        {
+            for (int i = 0; i < palya.GetLength(0); i++)
+            {
+                for (int j = 0; j < palya.GetLength(1); j++)
+                {
+                    palya[i, j] = '_';
+                }
+            }
+            Random rnd = new Random();
+            int sor, oszlop;
+            for (int i = 0; i < 10; i++)
+            {
+               do
+               {
+                  sor = rnd.Next(0, 10);
+                  oszlop = rnd.Next(0, 10);
+               } while (palya[sor, oszlop] == 'B');
+               palya[sor, oszlop] = 'B';
+            }
         }
     }
 }
